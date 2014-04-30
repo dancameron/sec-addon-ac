c53e87bdad2bf89f26ed0c3c1f9acab3692e86b1
Dan Cameron <dan@sproutventure.com>
2014-04-30 13:54:45 -0700

    Name the file after the class name.

--- classes/Group_Buying_Advanced_Coupons_Addon.php
+++ classes/Group_Buying_Advanced_Coupons_Addon.php
@@ -10,7 +10,7 @@ class Group_Buying_Advanced_Coupons_Addon extends Group_Buying_Controller {
 			'label' => self::__( 'Advanced Coupons' ),
 			'description' => self::__( 'Creates dropdown during deal creation to choose between deal or coupon and separates coupons into new template.' ),
 			'files' => array(
-				MW_ADVANCED_COUPONS_PATH . '/mw-advanced-coupons.controller.class.php',
+				MW_ADVANCED_COUPONS_PATH . '/Group_Buying_Advanced_Coupons.php',
 			),
 			'callbacks' => array(
 				array( 'Group_Buying_Advanced_Coupons', 'init' ),
